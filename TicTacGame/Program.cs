dbe2b69e7262972baadb355fba73b52ab5adb6b5
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-10 12:05:51 +0530

    Adding UC1 ticticGame to remote

---
+++ TicTacGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main method
+            Console.WriteLine("Welcome to The tic tac toe Game");
+            TicTacToeGame game = new TicTacToeGame();
+            game.DisplayBoardUC1();
+
+           
+        }
+    }
+}
---
+++ TicTacGame/TicTacToeGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacGame
+{
+    class TicTacToeGame
+    {
+        //static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        static char[] arr = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+        static int player = 1; 
+
+        static int choice;
+      
+        //displayiong the display board 
+        public void DisplayBoardUC1()
+        {
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
+        }
+
+
+    }
+
+   
+
+
+}
